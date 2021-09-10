388aecfaeab91fed8a5c3eb0e98fcf434177d71c
gaburolo <martinrolo22@estudiantec.cr>
2021-09-09 23:57:17 -0600

    [add] new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
+import logo from './logos/love_race_img.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faPlayCircle} from "@fortawesome/free-solid-svg-icons"
+import {songs} from "./appl";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+var keySong;
+var enable=false;
+var music = document.createElement("audio");
+var audio = document.createElement("source");
+
+const nameStyle = {color: 'black'};
+function song(id,url){
+  var tds = document.getElementsByClassName("rr");
+  keySong=id;
+  enable=true;
+  for(var i = 0; i < tds.length; i++) {
+     tds[i].style.color="black";
+  }
+  
+  document.getElementById(id).style.color='blue';
+
+  music = song_aux(url);
+ // console.log(document.getElementById('name').);
+  
+};
+
+
+function botonReproducir(){
+  if(enable===false){
+    alert("Seleccione la canción")
+  }else{
+    console.log(music);
+    music.play()
+  };
+};  
+function Letra(){
+  if(enable===false){
+    alert("Seleccione la canción")
+  }else{
+
+    const lyric= document.createElement("p");
+  
+    lyric.textContent=songs[keySong].lyrics;
+    console.log();
+    document.getElementById("lyricT").appendChild(lyric);
+    //document.getElementById("col-re2").textContent=songs[keySong].lyrics;
+  }
+  
+
+};   
+// El sonido que podemos reproducir o pausar
+
+
+function song_aux(url){
+
+  audio.src=url;
+  audio.setAttribute("type", "audio/mpeg");
+  music.setAttribute("controls","true");
+
+  music.appendChild(audio); 
+  document.body.appendChild(music);
+  music.style.display = "none";
+  return music;
 }
+function App() {
+
+    return (
+      <div className="App">
+        
+        <head className="App-header">
+          
+          <style>{"\
+            .rr{\
+              text-decoration: underline green;\
+            }\
+          "}
+          </style>
+          <style>{"\
+            .col-re2{\
+              display: flex;\
+              align-items:center;\
+            }\
+          "}
+          </style>
+          
+        </head>
+
+        
+        <body>
+        <audio id="myAudio">
+          
+          <source src="love_race.mp3" type="audio/mpeg"/>
+          Your browser does not support the audio element.
+        </audio>
+          <div class="container">
+            <div class="row">
+              <div class="col-re2">
+                
+                <img id="img-album" src={logo} width="300" alt=""/>
+                <p id="lyricT">Lyrics</p>
+                <audio id="player" controls=""></audio>
+                
+              </div> 
+              <div class="col-re2">
+                <p id="window">
+                <button class="player" onClick={(e) => botonReproducir()}  id="btnReproducir"><FontAwesomeIcon icon={faPlayCircle}/></button>
+                <button class="lyric" onClick={(e) => Letra()}  id="btnLyric">Lyric</button>
+                </p>
+              </div>
+              <div scr="stock-container" >
+              <table>
+                      <tr>
+                        <th>Name</th>
+                        <th>Artist</th>
+                      </tr>
+                {songs.map((data, key) => {
+                  return (
+                    
+                      <tr id="songs" >
+                        
+                        <td style={nameStyle} className='rr' id={key} onClick={(e) => song(key,data.song)}>{data.nombre }</td>
+                        
+                        <td  >{data.artist}</td>
+                      </tr>
+                    
+                    );
+                })}
+                </table>
+                
+              </div>
+  
+            </div>
+  
+          </div>
+          
+        </body>
+      </div>
+    );
+
+};
+  
+
 
 export default App;
---
+++ src/appl.js
@@ -0,0 +1,21 @@
+export const songs=[
+        
+            {
+                "id":0,
+                "nombre":"Love Race",
+                "artist":"Machine Gun Kelly",
+                "song":"https://s19.aconvert.com/convert/p3r68-cdx67/n1z46-7leh1.mp3",
+                "image":"logos/love_race_img.jpg",
+                "lyrics":"Na, da, da, da I found a silver ring And put it on my finger I picked up my guitar And played it for the Reaper God was a girl The devil wore a T-shirt Love is a game And they were kissing in the bleachers I don't want to be without you But I'm here, screaming by myself I'm on the run I'm on the run (I'm on the run) I'm on the run To get back to you (to get back) To get back to you I got a special ring And put it on her finger A voice said suddenly You're never gonna keep her Please don't look behind us The past is trying to catch up Pressures from outside us I've got claustrophobia I don't want to be without you But I'm here, screaming by myself I'm on the run I'm on the run (I'm on the run) I'm on the run To get back to you (to get back) To get back to you God was a girl The devil wore a T-shirt Love is a game And they were kissing in the bleachers God was a girl The devil wore a T-shirt Love is a game And they were kissing in the bleachers I'm on the run (I'm on the run, let's go) I'm on the run (to get back to you) I'm on the run (to get back to you) To get back to you (to get back) To get back to you I don't want to be without you But I'm here, screaming by myself I'm on the run (I'm on the run) To get back to you To get back to you Let's go Yeah To get back to you Uh, to get back, to get back To get back to you No, no, no, no No, no, no, no No, no, no, no"
+            },
+            {
+                "id":1,
+                "nombre":"Numb",
+                "artist":"Linkin Park",
+                "song":"https://assets.coderrocketfuel.com/pomodoro-times-up.mp3",
+                "image":"logos/love_race_img.mp3",
+                "lyrics":"cling"
+            }
+        
+
+];
